eaa47c6b0a3d4ad11b48aa74df5aa4e10229ad79
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-07-13 22:48:20 -0400

    added time

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    current_time = datetime.now().strftime("%H:%M:%S")
+    return render_template("index.html", time=current_time)
 
 if __name__ == "__main__":
     app.run(debug=True)
