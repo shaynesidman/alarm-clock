73b1d574e33d61880d29f3efd853c9f7085a48cc
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-07-26 10:47:03 -0400

    added function to get weather icon from short forecast

--- weather.py
+++ weather.py
@@ -30,6 +30,50 @@ def get_lat_lng():
     my_location = geocoder.ip("me")
     return my_location.latlng
 
+def get_weather_icon(shortForecast):
+    # Table mapping shortForecast in weather data to icon
+    condition_to_icon = {
+        "Severe storm": "rain",               # nsvrtsra
+        "Showers storms": "rain",             # scttsra
+        "Thunder storm": "rain",              # tsra
+        "Chance Snow/Rain": "snow",           # rasn
+        "Rain and Snow": "snow",              # rasn
+        "Rain or Snow": "snow",               # rasn
+        "Rain Sleet": "snow",                 # raip
+        "FrzgRn Snow": "snow",                # fzra_sn
+        "Freezing Rain": "snow",              # fzra
+        "Rain likely": "rain",                # ra
+        "Snow showers": "snow",               # sn
+        "Showers likely": "rain",             # shra
+        "Chance showers": "rain",             # shra
+        "Isolated showers": "rain",           # shra
+        "Scattered showers": "rain",          # shra
+        "Chance rain": "rain",                # ra
+        "Rain": "rain",                       # ra
+        "Mix": "snow",                        # rasn
+        "Sleet": "snow",                      # ip
+        "Snow": "snow",                       # sn
+        "Fog a.m.": "cloudy",                 # sctfg
+        "Fog late": "cloudy",                 # sctfg
+        "Fog": "cloudy",                      # fg
+        "Very Cold": "snow",                  # cold
+        "Very Hot": "sunny",                  # hot
+        "Hot": "sunny",                       # hot
+        "Overcast": "cloudy",                 # ovc
+        "Mostly Cloudy": "cloudy",            # bkn
+        "Partly Cloudy": "cloudy",            # sct
+        "Cloudy": "cloudy",                   # cloudy
+        "Partly Sunny": "cloudy",             # sct
+        "Mostly Sunny": "sunny",              # few
+        "Mostly Clear": "sunny",              # few
+        "Sunny": "sunny",                     # skc
+        "Clear": "sunny",                     # skc
+        "Fair": "sunny",                      # few
+        "Variable Clouds": "cloudy",          # bkn
+    }
+
+    return condition_to_icon[shortForecast]
+
 
 if __name__ in "__main__":
     get_weather()
